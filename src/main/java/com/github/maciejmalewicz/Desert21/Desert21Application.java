aa79aceb1f124e3ffbe80e7ee648a1947fff5863
Maciej Malewicz <maciekmalewicz2012b@gmail.com>
2022-02-05 13:16:30 +0100

    Initialised backend

---
+++ src/main/java/com/github/maciejmalewicz/Desert21/Desert21Application.java
@@ -0,0 +1,13 @@
+package com.github.maciejmalewicz.Desert21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desert21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desert21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maciejmalewicz/Desert21/Desert21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maciejmalewicz.Desert21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desert21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
