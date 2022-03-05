5e4ed0a04e0eaf9746f255bdc9ec2b91295b97ca
Maciej Malewicz <maciekmalewicz2012b@gmail.com>
2022-03-05 18:41:08 +0100

    Queuing players in progress

---
+++ src/main/java/com/github/maciejmalewicz/Desert21/controller/PlayersQueueController.java
@@ -0,0 +1,38 @@
+package com.github.maciejmalewicz.Desert21.controller;
+
+import com.github.maciejmalewicz.Desert21.exceptions.NotAcceptableException;
+import com.github.maciejmalewicz.Desert21.service.playersQueue.PlayersQueueService;
+import com.github.maciejmalewicz.Desert21.utils.AuthoritiesUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/queue")
+public class PlayersQueueController {
+
+    private final PlayersQueueService playersQueueService;
+
+    public PlayersQueueController(PlayersQueueService playersQueueService) {
+        this.playersQueueService = playersQueueService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addPlayerToTheQueue(Authentication authentication) throws NotAcceptableException {
+        var playersId = AuthoritiesUtils.getIdAuthorityFromAuthorities(authentication.getAuthorities())
+                        .orElseThrow(() -> new NotAcceptableException("Could not recognize the player!"));
+        playersQueueService.addPlayerToQueue(playersId);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping("/cancel")
+    @PostMapping
+    public ResponseEntity<Void> cancelPlayerQueue(Authentication authentication) throws NotAcceptableException {
+        var playersId = AuthoritiesUtils.getIdAuthorityFromAuthorities(authentication.getAuthorities())
+                .orElseThrow(() -> new NotAcceptableException("Could not recognize the player!"));
+        playersQueueService.removePlayerFromQueue(playersId);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/github/maciejmalewicz/Desert21/service/playersQueue/PlayersQueueService.java
+++ src/main/java/com/github/maciejmalewicz/Desert21/service/playersQueue/PlayersQueueService.java
@@ -3,24 +3,47 @@ package com.github.maciejmalewicz.Desert21.service.playersQueue;
 import com.github.maciejmalewicz.Desert21.service.gameGenerator.GameGeneratorService;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+//very basic queue, definitely will be improved when the game has trillions of players :)
 @Service
 public class PlayersQueueService {
 
     private String playerIdInQueue = null;
 
     private final GameGeneratorService gameGeneratorService;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public PlayersQueueService(GameGeneratorService gameGeneratorService) {
         this.gameGeneratorService = gameGeneratorService;
     }
 
-
-    public synchronized void addPlayerToQueue(String id) {
-        if (playerIdInQueue != null) {
-            gameGeneratorService.generateGame(playerIdInQueue, id);
-            playerIdInQueue = null;
-            return;
+    public void addPlayerToQueue(String id) {
+        lock.writeLock().lock();
+        try {
+            if (id.equals(playerIdInQueue)) {
+                return; //ignoring
+            }
+            if (playerIdInQueue != null) {
+                gameGeneratorService.generateGame(playerIdInQueue, id);
+                playerIdInQueue = null;
+                return;
+            }
+            this.playerIdInQueue = id;
+        } finally {
+            lock.writeLock().unlock();
         }
-        this.playerIdInQueue = id;
+    }
+
+    public synchronized void removePlayerFromQueue(String id) {
+        lock.writeLock().lock();
+        try {
+            if (id.equals(playerIdInQueue)) {
+                this.playerIdInQueue = null;
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 }
