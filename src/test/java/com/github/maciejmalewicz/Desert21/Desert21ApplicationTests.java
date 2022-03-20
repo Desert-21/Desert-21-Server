fae6eca3853916306ef65f86fcd7bb8042b5c079
Maciej Malewicz <maciej.malewicz.it@gmail.com>
2022-03-20 18:30:38 +0100

    Added properties in test

--- src/test/java/com/github/maciejmalewicz/Desert21/Desert21ApplicationTests.java
+++ src/test/java/com/github/maciejmalewicz/Desert21/Desert21ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.github.maciejmalewicz.Desert21;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Desert21ApplicationTests {
 
+	@Value("${spring.addresses.baseUrl}")
+	private String address;
+
 	@Test
 	void contextLoads() {
+		System.out.println(address);
 	}
 
 }
