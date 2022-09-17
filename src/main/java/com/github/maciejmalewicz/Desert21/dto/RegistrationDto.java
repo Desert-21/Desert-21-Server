fac0de568f6991dfc5a02f53083628cef07ad7c3
Maciej Malewicz <maciej.malewicz.it@gmail.com>
2022-09-17 15:01:06 +0200

    Added validation on registration

--- src/main/java/com/github/maciejmalewicz/Desert21/dto/RegistrationDto.java
+++ src/main/java/com/github/maciejmalewicz/Desert21/dto/RegistrationDto.java
@@ -2,10 +2,13 @@ package com.github.maciejmalewicz.Desert21.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record RegistrationDto (
-        String nickname,
-        String email,
-        String password
+        @NotBlank @Size(min = 2) @Size(max = 200) String nickname,
+        @NotBlank @Size(min = 2) @Size(max = 200) String email,
+        @NotBlank @Size(min = 8) @Size(max = 200) String password
 ) {
 }
